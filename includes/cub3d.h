69021a5089138b03d53a22fe1fbd11668af6ff59
Anas Abouyazid <aabouyaz@student.42paris.fr>
2025-10-13 12:36:01 +0200

    merge elena

---
+++ includes/cub3d.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edurance <edurance@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/12 14:44:14 by edurance          #+#    #+#             */
+/*   Updated: 2025/10/13 12:22:54 by edurance         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# define SIZE_X 800
+# define SIZE_Y 600
+
+# include "../libft/include/libft.h"
+# include "../mlx_linux/mlx.h"
+# include <fcntl.h>
+# include <stdio.h>
+
+typedef struct s_ply
+{
+	float	posX;
+	float	posY;
+	float	dirX;
+	float	dirY;
+}			t_ply;
+
+typedef struct s_cub
+{
+	void	*mlx;
+	void	*mlx_window;
+	char	**map;
+	int		longest_line;
+	int		nb_lines;
+	int		mapcub_size;
+	int		map_y;
+	int		map_x;
+	t_ply	*player;
+}			t_cub;
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+/*minimap*/
+int			get_mapsize(char *mapfile, t_cub *cub);
+void		get_mapdata_display(t_cub *cub);
+void		create_map(t_cub *cube, char *mapfile);
+
+/*utils*/
+int			ft_color(int r, int g, int b);
+
+/*player*/
+int			init_player_data(t_cub *cube);
+
+#endif
---
+++ minimap/minimap_data.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minimap_data.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edurance <edurance@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 11:12:12 by edurance          #+#    #+#             */
+/*   Updated: 2025/10/13 12:12:39 by edurance         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+/*recupere la longest line et le nb de lignes*/
+int	get_mapsize(char *mapfile, t_cub *cub)
+{
+	int		nb_lines;
+	int		lg_len;
+	int		fd;
+	int		line_len;
+	char	*line;
+
+	nb_lines = 0;
+	lg_len = 0;
+	fd = open(mapfile, O_RDONLY);
+	if (fd == -1)
+		return (1);
+	line = get_next_line(fd);
+	while (line)
+	{
+		line_len = (int)ft_strlen(line) - 1;
+		if (lg_len < line_len)
+			lg_len = line_len;
+		nb_lines++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	cub->longest_line = lg_len;
+	cub->nb_lines = nb_lines;
+	return (0);
+}
+
+/*quelle taille pour un cube ? a quelle position afficher la map ?*/
+void	get_mapdata_display(t_cub *cub)
+{
+	int	cub_x;
+	int	cub_y;
+
+	cub_x = SIZE_X / cub->longest_line;
+	cub_y = SIZE_Y / cub->nb_lines;
+	if (cub_x < cub_y)
+	{
+		cub->map_x = 0;
+		cub->map_y = (SIZE_Y - cub_x * cub->nb_lines) / 2;
+		cub->mapcub_size = cub_x;
+	}
+	else if (cub_x > cub_y)
+	{
+		cub->map_y = 0;
+		cub->map_x = (SIZE_X - cub_y * cub->longest_line) / 2;
+		cub->mapcub_size = cub_y;
+	}
+	else
+	{
+		cub->map_x = 0;
+		cub->map_y = 0;
+		cub->mapcub_size = cub_x;
+	}
+}
+
+/*creer le char ** avec la map*/
+void	create_map(t_cub *cube, char *mapfile)
+{
+	int		fd;
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (get_mapsize(mapfile, cube))
+		return ;
+	cube->map = malloc(sizeof(char *) * cube->nb_lines + 1);
+	if (!cube->map)
+		return ;
+	fd = open(mapfile, O_RDONLY);
+	if (fd == -1)
+		return ;
+	line = get_next_line(fd);
+	while (line)
+	{
+		line[ft_strlen(line) - 1] = 0;
+		cube->map[i] = line;
+		i++;
+		line = get_next_line(fd);
+	}
+	close(fd);
+}
